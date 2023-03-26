75a5ea84930b248e3f00f52ac50639c112c4c069
A Lakshmi Prathyusha <120932561+AripakaLakshmiPrathyusha11@users.noreply.github.com>
2023-03-26 03:30:59 +0000

    adding server pages

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def Helloworld():
+def home_page():
   return "Hello aspiring data scientists!"
+
+def search_
   
 if __name__ == '__main__':
   app.run(host='0.0.0.0',debug=True)
